ab0ad83a1f2c001f3975fbd511e32b5fd5305f10
shoaib-eth <shoaib.developer69@gmail.com>
2026-02-04 11:57:16 +0530

    Solution - 3

---
+++ 19_Decorators/02_Solution.py
@@ -0,0 +1,24 @@
+def debug(func):
+    def wrapper(*args, **kwargs):
+        args_value = " ,".join(str(arg) for arg in args)
+        kwargs_value = " ,".join(f"{k} : {v}" for k, v in kwargs.items())
+        print(
+            f"Calling: {func.__name__} with args {args_value} and kwargs {kwargs_value}"
+        )
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@debug
+def hello():
+    print("Hello 👋 World!")
+
+
+@debug
+def greet(name, greeting="Hello 👋"):
+    print(f"{greeting}, {name}")
+
+
+hello()
+greet("Alice", greeting="Hola")
